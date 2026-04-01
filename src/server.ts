08a0fde29150d6dfc5dd2edbf80a819097492d9a
Clara Nascimento <clara.nasc13@gmail.com>
2026-04-01 19:38:26 -0300

    feat: initialize express server with GET and POST product routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,31 @@
 import express from "express";
 
-const app = express()
-
 const PORT = 3333
 
+const app = express()
+
+// habilitar o uso de json no express
+app.use(express.json())
+
+// rota raiz com query params 
+app.get("/products", (req, res) => {
+    // query params: ?page=1&limit=10
+    const {page,limit} = req.query
+
+    //passando parametros diretamente na rota
+    res.send(`Página: ${page} | Limite: ${limit}`)
+})
+
+app.post("/products", (req, res) => {
+    // body params: { "name": "Product 1", "price": 10 }
+    const {name,price} = req.body
+//res.send(`Produto: ${name} | Preço: ${price}`)
+
+//da pra passar a reposta direto em json, sem precisar fazer stringfy
+    res.status(201).json({name,price})
+})
+
+// inicializa o servidor
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
